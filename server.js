c389491aa5f17c08d3deccc58b757bd5a99b039f
Miguel angel morales <miguel.moralesr@hotmail.com>
2019-07-31 00:09:23 -0500

    1.0.6

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-app.use(express.static(path.join(__dirname, "./dist/")));
-
+app.use(express.static(path.join(__dirname, "./dist/tiempos/")));
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, './dist/tiempos/index.html'))
+})
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);

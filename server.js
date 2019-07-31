fef5ad1745ccbaefba25de838f8244d85a5042de
Miguel angel morales <miguel.moralesr@hotmail.com>
2019-07-30 23:52:45 -0500

    1.0.5

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-app.use(express.static(path.join(__dirname, "./dist/tiempos")));
+app.use(express.static(path.join(__dirname, "./dist/tiempos/")));
 app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, './dist/tiempos/index.html'))
 })
